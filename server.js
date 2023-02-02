458067c4d54b3491afb5955ce0b635bb50cd1d22
minji.jo <mwinterj@daum.net>
2023-02-02 12:08:00 +0900

    :hammer: Chore : 서버 포트 8000으로 변경
    Co-authored-by: DADA6061 <d.ya96v@gmail.com>
    Co-authored-by: IntHyun <wjdtngus1237@naver.com>

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 5500, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log("server....!");
 });
