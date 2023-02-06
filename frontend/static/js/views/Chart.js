d295e11eee53261d8ac32fdc3af816a62865938d
minji.jo <mwinterj@daum.net>
2023-02-06 11:44:07 +0900

    :sparkles: Feat: 차트 화면 요소 생성
    Co-authored-by: DADA6061 <d.ya96v@gmail.com>
    Co-authored-by: IntHyun <wjdtngus1237@naver.com>

--- frontend/static/js/views/Chart.js
+++ frontend/static/js/views/Chart.js
@@ -1,4 +1,5 @@
 import AbstractView from "./AbstractView.js";
+import ChartTop from "./ChartTop.js";
 
 export default class Chart extends AbstractView {
   constructor($target) {
@@ -14,6 +15,65 @@ export default class Chart extends AbstractView {
     const wrapper = document.createElement("main");
     wrapper.classList.add("wrapper", "chart-wrapper");
     this.$target.appendChild(wrapper);
+
+    const chartWrapper = document.createElement("section");
+    const chartMain = document.createElement("section");
+    const chartTitle = document.createElement("h2");
+    const chartHeader = document.createElement("ul");
+    chartTitle.classList.add("sr-only");
+    chartHeader.classList.add("chart-header");
+    chartTitle.textContent = "차트 화면";
+    chartWrapper.append(chartTitle, chartHeader);
+    wrapper.append(chartWrapper, chartMain);
+
+    const chartHeaderItemList = [
+      { top20: "TOP 20" },
+      { genre1: "장르1" },
+      { genre2: "장르2" },
+    ];
+
+    chartHeaderItemList.forEach((listTitle) => {
+      const chartHeaderItem = document.createElement("li");
+      const chartHeaderItemLink = document.createElement("a");
+      chartHeaderItemLink.setAttribute(
+        "href",
+        `/chart/${Object.keys(listTitle)[0]}`
+      );
+      chartHeaderItem.appendChild(chartHeaderItemLink);
+      chartHeaderItem.classList.add("chart-header-item");
+      chartHeaderItemLink.textContent = Object.values(listTitle)[0];
+      console.log(Object.values(listTitle)[0]);
+      chartHeader.appendChild(chartHeaderItem);
+    });
+
+    chartMain.classList.add("chart-main");
+    const chartMainTitle = document.createElement("h3");
+    const chartMainHeader = document.createElement("ol");
+    chartMainTitle.classList.add("sr-only");
+    chartMain.append(chartMainTitle, chartMainHeader);
+
+    this.getItemList();
+  }
+
+  getItemList() {
+    const chartHeaderItem = document.querySelector(".chart-header-item");
+    const chartMainHeader = document.querySelector(".chart-main ol");
+
+    chartHeaderItem.classList.toggle("header-active");
+    chartMainHeader.innerHTML = `
+    <li class="chart-list-item">
+      <span class="chart-top-rank">1</span>
+      <figure class="chart-album-info">
+        <img src="./static/image/album-img.png" alt="앨범 타이틀">
+        <figcaption class="chart-item-info">
+          <span class="chart-item-title">19th Floor</span>
+          <span class="chart-item-artist">Bobby Richards</span>
+        </figcaption>
+      </figure>
+      <button class="chart-btn-play" type="button"><img src="./static/image/icon-play.svg" alt="재생버튼"></button>
+      <button type="button"><img src="./static/image/icon-plus.svg" alt="추가버튼"></button>
+    </li>
+  `;
   }
 }
 
