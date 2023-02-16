ccebd614c431f8c64cae15fb45326a00f847f2b1
JeongSuHyun <68059880+baejeong12@users.noreply.github.com>
2023-02-16 10:49:21 +0900

    :sparkles: Feat: 홈 화면 fetch 데이터 카테고리별 랜덤으로 렌더링
    Co-authored-by: DADA6061 <d.ya96v@gmail.com>
    Co-authored-by: ming-Jo <mwinterj@daum.net>

--- frontend/static/js/views/Home.js
+++ frontend/static/js/views/Home.js
@@ -22,34 +22,36 @@ export default class Home extends AbstractView {
 
     const homeTitle = document.createElement("h2");
     const advertise = document.createElement("aside");
-    const contCategory = document.createElement("section");
     homeTitle.classList.add("sr-only");
     homeTitle.textContent = "홈 화면";
     advertise.textContent = "우리 광고";
-    contCategory.classList.add("cont-category");
 
-    const categoryTitle = document.createElement("h3");
-    const contAlbum = document.createElement("div");
-    categoryTitle.classList.add("tit-category");
-    contAlbum.classList.add("cont-album");
+    homeWrapper.append(homeTitle, advertise);
 
     this.getArtistData();
-
-    // categoryTitle.textContent = "최신곡";
-    // contAlbum.innerHTML = `
-    //   <a class="cont-album-link" href="/playcontrol">
-    //     <img class="cover-album" src="/static/image/album-img.png" alt="앨범커버" />
-    //   </a>
-    // `;
-
-    contCategory.append(categoryTitle, contAlbum);
-    homeWrapper.append(homeTitle, advertise, contCategory);
   }
 
   pickArtistData() {
     const koreanArtist = ["아이유", "BTS", "성시경", "뉴진스", "임영웅", "태연", "세븐틴", "블랙핑크"];
-    const foreignArtist = ["Ariana Grande", "eminem", "Justin Bieber", "Charlie Puth", "Taylor Swift", "Rihanna", "Beyonce"];
-    const femaleArtist = ["소녀시대", "마마무", "청하", "Lady Gaga", "Alicia Keys", "Kesha", "Kelly Clarkson", "Billie Eilish"];
+    const foreignArtist = [
+      "Ariana Grande",
+      "eminem",
+      "Justin Bieber",
+      "Charlie Puth",
+      "Taylor Swift",
+      "Rihanna",
+      "Beyonce",
+    ];
+    const femaleArtist = [
+      "소녀시대",
+      "마마무",
+      "청하",
+      "Lady Gaga",
+      "Alicia Keys",
+      "Kesha",
+      "Kelly Clarkson",
+      "Billie Eilish",
+    ];
     const maleArtist = ["싸이", "폴킴", "박재범", "케이윌", "Sam Smith", "Ed Sheeran", "Maroon5"];
 
     const artistListGroup = [koreanArtist, foreignArtist, femaleArtist, maleArtist];
@@ -57,10 +59,7 @@ export default class Home extends AbstractView {
     let artistRandomPick = [];
 
     artistListGroup.map((artistList) => {
-      artistRandomPick.push(
-        artistList[Math.floor(Math.random() * artistList.length)]
-      );
-    console.log("artistList", artistRandomPick)
+      artistRandomPick.push(artistList[Math.floor(Math.random() * artistList.length)]);
     });
 
     return artistRandomPick;
@@ -68,78 +67,47 @@ export default class Home extends AbstractView {
 
   getArtistData() {
     let randomArtistArr = this.pickArtistData();
-    
+
     let tempData = [];
     randomArtistArr.map((artist) => {
-      useFetch(`search?q=${artist}`)
-        .then((data) => {
-          tempData.push(data)
-        // console.log(data);
-        if(tempData.length === 4){
+      useFetch(`search?q=${artist}`).then((data) => {
+        tempData.push(data);
+        if (tempData.length === 4) {
           this.repeatCreate(tempData);
         }
       });
     });
-
   }
 
   repeatCreate(musicData) {
-    // let data = [];
     let data = musicData;
-    // data.push(musicData);
-    console.log("musicData", data);
 
-    const $categoryTitle = document.querySelector(".tit-category");
-    console.log($categoryTitle)
-    $categoryTitle.textContent = "최신곡";
-    const $contAlbum = document.querySelector(".cont-album");
-    let albumLink = data[0].map((item) => {
-      // console.log(item)
-      return `
-      <a class="cont-album-link" href="/playcontrol">
-        <img
-          class="cover-album"
-          src=${item.album.cover_medium}
-          alt="앨범커버"
-        />
-      </a>
+    const category = ["이 노래 어때요?", "나만 알고 싶은 노래", "20대 취향저격", "오늘의 인기 가수"];
+    const $homeWrapper = document.querySelector(".home-wrapper");
+
+    const test = data
+      .map((list, idx) => {
+        return `
+      <section class="cont-category">
+        <h3 class="tit-category">${category[idx]}</h3>
+        <div class="cont-album">
+          ${list
+            .map((item) => {
+              return `
+                <a class="cont-album-link" href="#">
+                  <img class="cover-album" src=${item.album.cover_medium} alt="앨범커버" />
+                  <p class="album-title">${item.title}</p>
+                  <p>${item.artist.name}</p>
+                </a>
+            `;
+            })
+            .join("")}
+        </div>
+      </section>
       `;
-    }).join("");
-    $contAlbum.innerHTML = albumLink;
+      })
+      .join("");
+
+    $homeWrapper.innerHTML += test;
   }
 }
-
-// return `
-//   <main class="wrapper">
-//   <section class="home-wrapper">
-//     <h2 class="sr-only">홈 화면</h2>
-//     <aside>우리광고</aside>
-//     <section class="cont-category">
-//       <h3 class="tit-category">최신곡</h3>
-//       <div class="cont-album">
-//         <a class="cont-album-link" href="#">
-//           <img class="cover-album" src="/static/image/album-img.png" alt="앨범커버" />
-//         </a>
-//       </div>
-//     </section>
-
-//     <section class="cont-category">
-//       <h3 class="tit-category">오늘의 Best</h3>
-//       <div class="cont-album">
-//         <a class="cont-album-link" href="#">
-//           <img class="cover-album" src="/static/image/album-img.png" alt="앨범커버" />
-//         </a>
-//       </div>
-//     </section>
-
-//     <section class="cont-category">
-//       <h3 class="tit-category">오늘의 선곡(추천)</h3>
-//       <div class="cont-album">
-//         <a class="cont-album-link" href="#">
-//           <img class="cover-album" src="/static/image/album-img.png" alt="앨범커버" />
-//         </a>
-//       </div>
-//     </section>
-//   </section>
-// </main>
-// `;
